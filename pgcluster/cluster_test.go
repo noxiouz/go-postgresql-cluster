9a16e650b5ceac6086ef58745e1d2c6548cb50f5
Anton Tiurin <noxiouz@yandex.ru>
2016-02-13 03:14:11 +0300

    test: add simple test cases for errors

--- pgcluster/cluster_test.go
+++ pgcluster/cluster_test.go
@@ -2,6 +2,7 @@ package pgcluster
 
 import (
 	"testing"
+	"time"
 
 	_ "github.com/lib/pq"
 
@@ -29,4 +30,41 @@ func (s *ClusterSuite) TestNewCluster(c *C) {
 	db := cluster.DB(MASTER)
 	err = db.Ping()
 	c.Assert(err, IsNil)
+}
+
+func (s *ClusterSuite) TestZeroDataSourceError(c *C) {
+	_, err := NewPostgreSQLCluster("postgres", []string{})
+	c.Assert(err, Equals, ErrZeroDataSource)
+}
+
+func (s *ClusterSuite) TestDublicatedDataSource(c *C) {
+	var connStrings = []string{
+		"user=ubuntu dbname=circle_test",
+		"user=ubuntu dbname=circle_test",
+	}
+
+	_, err := NewPostgreSQLCluster("postgres", connStrings)
+	c.Assert(err, Equals, ErrDublicatedDataSource)
+}
+
+func (s *ClusterSuite) TestOverWatch(c *C) {
+	var connStrings = []string{
+		"user=ubuntu dbname=circle_test",
+		"user=user1 dbname=testdb",
+	}
+
+	cluster, err := NewPostgreSQLCluster("postgres", connStrings)
+	c.Assert(err, IsNil)
+	defer cluster.Close()
+
+	db1 := cluster.DB(MASTER)
+	err = db1.Ping()
+	c.Assert(err, IsNil)
+
+	time.Sleep(time.Second * 10)
+	db2 := cluster.DB(MASTER)
+	err = db2.Ping()
+	c.Assert(err, IsNil)
+
+	c.Assert(db1 == db2, Equals, true)
 }
