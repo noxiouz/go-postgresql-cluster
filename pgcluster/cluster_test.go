f6903277bd0967b73bea27971dbff07d3b13653b
Anton Tiurin <noxiouz@yandex.ru>
2016-02-15 20:53:31 +0300

    test: Use docker-compose to start testing PG cluster

--- pgcluster/cluster_test.go
+++ pgcluster/cluster_test.go
@@ -1,6 +1,8 @@
 package pgcluster
 
 import (
+	"fmt"
+	"os/exec"
 	"testing"
 	"time"
 
@@ -18,11 +20,51 @@ func init() {
 
 type ClusterSuite struct{}
 
-func (s *ClusterSuite) TestNewCluster(c *C) {
-	var connStrings = []string{
-		"user=ubuntu dbname=circle_test",
-		"user=user1 dbname=testdb",
+const (
+	composeFile = "../tests/docker-compose.yml"
+)
+
+// Look at tests/docker-compose.yml
+func getConnStrings() []string {
+	var (
+		dockerMachineName = "dev"
+		host              = ""
+	)
+
+	// MacOS X: detect docker-machine IP
+	if output, err := exec.Command("docker-machine", "ip", dockerMachineName).Output(); err == nil {
+		host = fmt.Sprintf(" host=%s", output)
 	}
+
+	// look at tests/docker-compose.yml
+	return []string{
+		// slave
+		"user=dbuser dbname=dbname password=dbuserpass sslmode=disable port=7432" + host,
+		// master
+		"user=dbuser dbname=dbname password=dbuserpass sslmode=disable port=6432" + host,
+	}
+}
+
+func (s *ClusterSuite) SetUpSuite(c *C) {
+	//docker-compose -f tests/docker-compose.yml up -d
+	output, err := exec.Command("docker-compose", "-f", composeFile, "up", "-d", "--force-recreate").CombinedOutput()
+	if err != nil {
+		c.Fatalf("unable to start PostgreSQL cluster via compose: %v. Output %s", err, output)
+	}
+	// TODO: poll
+	time.Sleep(10 * time.Second)
+}
+
+func (s *ClusterSuite) TearDownSuite(c *C) {
+	//docker-compose -f tests/docker-compose.yml stop
+	output, err := exec.Command("docker-compose", "-f", composeFile, "stop").CombinedOutput()
+	if err != nil {
+		c.Fatalf("unable to stop PostgreSQL cluster via compose: %v. Output %s", err, output)
+	}
+}
+
+func (s *ClusterSuite) TestNewCluster(c *C) {
+	var connStrings = getConnStrings()
 	cluster, err := NewPostgreSQLCluster("postgres", connStrings)
 	c.Assert(err, IsNil)
 	defer cluster.Close()
@@ -48,10 +90,7 @@ func (s *ClusterSuite) TestDublicatedDataSource(c *C) {
 }
 
 func (s *ClusterSuite) TestOverWatch(c *C) {
-	var connStrings = []string{
-		"user=ubuntu dbname=circle_test",
-		"user=user1 dbname=testdb",
-	}
+	var connStrings = getConnStrings()
 
 	cluster, err := NewPostgreSQLCluster("postgres", connStrings)
 	c.Assert(err, IsNil)
